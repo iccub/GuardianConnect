091d9cff25de47ffdbdc3c5d99e4cb4674231d1d
Michał Buczek <michal.buczek@protonmail.com>
2022-07-04 19:51:04 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,7 @@
-//
+// swift-tools-version:5.6
 //  Package.swift
 //  GuardianConnect
 //
-// swift-tools-version:5.6
 //
 
 import PackageDescription
@@ -10,13 +9,15 @@ import PackageDescription
 let package = Package(
 	name: "GuardianConnect",
 	platforms: [
-		.macOS(.v10.15),
+		.macOS(.v10_15),
 		.iOS(.v13)
 	],
 	products: [
-		.library(name: "GuardianConnect", targets: ["GuardianConnect", "GuardianConnectMac"])
+		.library(name: "GuardianConnect", targets: ["GuardianConnect"])
 	],
 	targets: [
-		.binaryTarget(name: "GuardianConnect", url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.6/GuardianConnect.xcframework.zip")
+		.binaryTarget(name: "GuardianConnect", 
+			      url:"https://github.com/GuardianFirewall/GuardianConnect/releases/download/1.6/GuardianConnect.xcframework.zip", 
+			      checksum: "418c1a739ab0a09e9aad28a9f4a49d9dee80a5accbcdf5ad3291a706f97f01d2")
 	]
 )
